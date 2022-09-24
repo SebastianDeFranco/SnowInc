198068941e41ade1a0da63042995115898ff9104
SebastianDeFranco <sebastianedefranco@gmail.com>
2022-09-24 15:39:42 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let textWrapper = document.querySelector('.hero__title .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+.add({
+    targets: '.hero__title .letter',
+    translateY: ["1.1em", 0],
+    translateX: ["0.55em", 0],
+    translateZ: 0,
+    rotateZ: [180, 0],
+    duration: 750,
+    easing: "easeOutExpo",
+    delay: (el, i) => 50 * i
+})
